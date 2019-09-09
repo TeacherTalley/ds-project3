46ae93907a3433915cad5b0e67344e189178bd1a
TeacherTalley <49789063+TeacherTalley@users.noreply.github.com>
2019-09-09 12:50:45 -0500

    Small comment added to header file

--- BSTArray.h
+++ BSTArray.h
@@ -2,3 +2,4 @@
 #include "ElementType.h"
 // Create BSTArray class defintiion here
 // Remember, your underlying data type will be an array, not a linked list!
+// Don't forget class definitions end with a semi-colon!
